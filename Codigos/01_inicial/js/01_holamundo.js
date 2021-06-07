f672e78171b48f11d61891820b8708aa1c393d32
Mario-Munoz-Barroso <72133132+Mario-Munoz-Barroso@users.noreply.github.com>
2021-06-07 12:40:21 +0200

    asd

---
+++ Codigos/01_inicial/js/01_holamundo.js
@@ -0,0 +1,38 @@
+// //comentario simple
+// /* comentario multiple*/
+// console.log("Mensaje de comprobacion primer proyecto");
+// console.log("Hola mundo");
+// console.log("Esto es otra linea");
+// console.log(1, 2, 3, 4, 5, 5, 67, 7, 5, 3, 2);
+// console.time("Iniciojs");
+// //Promesas
+// console.timeEnd("Inicio JS");
+// //Variables
+// // tipo(acceso) nombre = valor
+//  const nombre = "Borja"
+//  (((((MMMAAAAALLLL!!!!))))) nombre = "otro" No puedes cambiar el valor a una constante
+// console.log ("Mi nombre es "+nombre)
+// var edad = 19 // en esta si se puede cambiar el valor
+// let experiencia = true
+
+// let nombreString = "nombre";
+// let EdadNumber = 13;
+// let EdadNumberDec = 13.4;
+// let experienciaBoolean = false;
+// let cualquierNull = null;
+// let cosaSinDefinir = undefined;
+// let cosaColeccion = [[], [], []];
+// let cosaColeccion = [];
+let variableObjeto = {
+  Nombre: "Borja",
+  Edad: 19,
+  experiencias: true,
+  hoobies: ["futbol", "cine"]
+};
+
+let fecha = new Date;
+let palabraObject = new String ("Nombre Objeto")
+
+console.log("Mi nombre es: "+Nombre+"y tengo "+edad+" y tengo experiencia: "+experiencia);
+console.log(`Mi nombre es : ${Nombre} y tengo ${edad} y tengo experiencia ${experiencia}`);
+console.log("Mi nombre es %s y tengo %d y mi experiencia es %b", Nombre, edad, experiencia);
